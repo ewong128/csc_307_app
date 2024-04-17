529ff18a4a7899906ed25ce068abd3a127a4ee0d
ewong128 <ewong128@calpoly.edu>
2024-04-17 14:01:23 -0700

    Updated files for Step 6 of IE2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,7 +40,12 @@ const users = {
       id: "zap555",
       name: "Dennis",
       job: "Bartender"
-    }
+    },
+	{
+  "id": "qwe123",
+  "job": "Zookeeper",
+  "name": "Cindy"
+}
   ]
 };
 
@@ -73,6 +78,17 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
